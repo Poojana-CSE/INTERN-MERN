0779aeca6076de81696e4dcf125c9146b3845e66
Poojana-CSE <poojana.s2023cse@sece.ac.in>
2025-01-10 12:04:08 +0530

    Guess It COmpleted

--- JS/Guess It/index.js
+++ JS/Guess It/index.js
@@ -1,31 +1,29 @@
 var randomNumber=Math.floor(Math.random()*100)+1;
 var attempts=10;
 console.log(randomNumber);
-var bc=0;
+var boxcount=0;
 
 document.getElementById('btn').addEventListener('click',function(){
      const guess=parseInt(document.getElementById('guessInput').value);
      const lock=document.getElementById('lock');
       if(guess===randomNumber){
-        bc++;
+        boxcount++;
         lock.src='uk.avif';
         lock.width = 249; 
         lock.height = 209;
-        display("Congratulations!!!.You opened the box"+bc);
+        display("Congratulations!!!.You opened the box "+boxcount);
         randomNumber=Math.floor(Math.random()*100)+1;
         attempts=10;
         setTimeout(()=>{
           lock.src='lock.jpg';
-          display("Guess the number to open the Box "+(bc + 1));
+          display("Guess the number to open the Box "+(boxcount + 1));
           document.getElementById('guessInput').value='';
         },5000);
-        if(bc===5){
+        if(boxcount===5){
           display("Congratulations! You won the game!");
           document.getElementById('btn').disabled=true;
           document.getElementById('guessInput').disabled=true;
-        }
-
-       
+        }       
       }
       else if(guess<randomNumber){
         display("Your Number is too low..Try Again.");
@@ -33,14 +31,12 @@ document.getElementById('btn').addEventListener('click',function(){
       else{
         display("Your Number is too high..Try Again.");
       }
-    
     attempts--;
     if(attempts===0){
       display('Game Over! Try Again Later');
   }
-
 });
 
- function display(msg){
+function display(msg){
 document.getElementById('msg').textContent=msg;
  }
